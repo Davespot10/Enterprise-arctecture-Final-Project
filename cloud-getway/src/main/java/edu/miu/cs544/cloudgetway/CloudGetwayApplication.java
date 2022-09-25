8ddcd368df8039e207440173141560d11edf7af3
Natty Teshome <nattyteshome30@gmail.com>
2022-09-25 18:37:46 -0500

    all in one

---
+++ cloud-getway/src/main/java/edu/miu/cs544/cloudgetway/CloudGetwayApplication.java
@@ -0,0 +1,23 @@
+package edu.miu.cs544.cloudgetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGetwayApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
